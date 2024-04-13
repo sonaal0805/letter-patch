3e921030860a8b37d55d65595ebb9269ef0b866a
sonal0805 <sonal.chandra@lojel.com>
2024-04-13 16:14:57 +0800

    enable touch

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
 function dragElement(element) {
     
     element.onmousedown = dragMouseDown;
+    element.ontouchstart = dragMouseDown;
 
     function dragMouseDown(e) {
         e.preventDefault();
@@ -14,14 +15,19 @@ function dragElement(element) {
         pos3 = e.clientX;
         pos4 = e.clientY;
         document.onmouseup = closeDragElement;
+        document.touchend = closeDragElement;
         // call a function whenever the cursor moves:
         document.onmousemove = elementDrag;
+        document.ontouchmove = elementDrag;
+        
     }
     
     function closeDragElement() {
         /* stop moving when mouse button is released:*/
         document.onmouseup = null;
+        document.touchend = null;
         document.onmousemove = null;
+        document.ontouchmove = null;
     }
     
     function elementDrag(e) {
